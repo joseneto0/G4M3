9a038f4987e8cd4d4fe2a1628c037b64e61fea0c
Jose <joseneto.jn1606@gmail.com>
2022-09-04 20:28:39 -0300

    G4M3

--- P0046.cpp
+++ P0046.cpp
@@ -2,35 +2,41 @@
 
 using namespace std;
 
-int mochila(vector<int>qtdEstrelas, vector<int> valorEstrelas, int qtdObjetos, int valorMochila, int cont){
-    int x[qtdObjetos];
-    for (int i = qtdObjetos; i > 0; i--){
-        if (qtdEstrelas[i] <= valorMochila){
-            x[i] = 1;
-            valorMochila -= qtdEstrelas[i];
-        } else {
-            x[i] = valorMochila / (qtdEstrelas[i]);
-            valorMochila = 0;
-        }
-    }
-    return x[cont];
-}
-
 int main(void){
-    int n, m, q, v, cont=0, soma=0;
+    int n, m, q, v, cont=0;
     cin >> n >> m;
     vector<int> numEstrelas;
     vector<int> valorNutritivo;
+    vector<int> valorNutritivo2;
     for (int i = 0; i < n; i++){
         cin >> q >> v;
         numEstrelas.push_back(q);
         valorNutritivo.push_back(v);
     }
-    for (int i = 0; i < n; i++){
-        soma += mochila(numEstrelas, valorNutritivo, n, m, cont);
-        cout << soma << endl;
-        cont++;
-    }
-    cout << soma << endl;
+	for (int i = 0; i < numEstrelas.size(); i++){
+		if (numEstrelas[i] > 1){
+			while (numEstrelas[i] != 0){
+				valorNutritivo2.push_back(valorNutritivo[i]);
+				numEstrelas[i] -= 1;
+			}
+		} else {
+			valorNutritivo2.push_back(valorNutritivo[i]);
+		}
+	}
+	sort(valorNutritivo2.begin(), valorNutritivo2.end());
+    reverse(valorNutritivo2.begin(), valorNutritivo2.end());
+	vector<int> numEstrelas2(valorNutritivo2.size());
+	for (int i = 0; i < valorNutritivo2.size(); i++){
+		numEstrelas2[i] = 1;
+	}
+	sort(numEstrelas2.begin(), numEstrelas2.end());
+	reverse(numEstrelas2.begin(), numEstrelas2.end());
+	int i = 0;
+	while (m > 0){
+		cont += valorNutritivo2[i];
+		m--;
+		i++;
+	}
+    cout << cont << '\n';
     return 0;
 }
